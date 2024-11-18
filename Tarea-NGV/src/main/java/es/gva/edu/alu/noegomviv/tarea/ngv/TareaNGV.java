013eca59ad9ac8f24a6d71f1b7d96902ed9f7c45
Noelia Gomez Vives <noegomviv@alu.edu.gva.es>
2024-11-18 19:37:23 +0100

    Creación de nuevo proyecto Maven con Java: Tarea-NGV Fix #2

---
+++ Tarea-NGV/src/main/java/es/gva/edu/alu/noegomviv/tarea/ngv/TareaNGV.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package es.gva.edu.alu.noegomviv.tarea.ngv;
+
+/**
+ *
+ * @author Noelia
+ */
+public class TareaNGV {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
